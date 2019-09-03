807edab7dec91ada980ce83c67fd8d6f42d81a2f
mtinglof <42756027+mtinglof@users.noreply.github.com>
2019-09-03 11:52:05 -0700

    final edits

--- submittimecreature.py
+++ submittimecreature.py
@@ -8,6 +8,7 @@ import numpy as np
 import rpy2
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
+from rpy2.rinterface import RRuntimeError
 
 import warnings
 import os
@@ -22,6 +23,14 @@ class SubmitCreature():
         self.fname = os.path.join(self.path_to_data, self.file_name)
         self.full_data_set = []
         self.test_set = []
+
+        self.time_mean_sd = pd.read_csv('time_series_mean_sd.csv')
+        base = importr('base')
+        utils = importr('utils')
+        utils.chooseCRANmirror(ind=1) 
+        utils.install_packages('forecast')
+        self.arima = robjects.r['arima']
+
         self.getData()
 
     def getTestData(self): 
@@ -61,7 +70,7 @@ class SubmitCreature():
             yield obj
 
     def testCreature(self, creature, file_name): 
-        creature_input = self.getTransformedCreatureData()
+        creature_input = self.transformed_data 
         model = Neural_Network(creature.input_length, creature.hidden_length)
         model.setWeights(creature.getCreature()["weights"])
         predict = model.forward(torch.tensor(creature_input, dtype=torch.float))
@@ -70,10 +79,12 @@ class SubmitCreature():
         np.savetxt(saved_file_name, np_predict, delimiter=",")
 
     def getTransformedCreatureData(self): 
-        full_data = self.training_set
+        full_data = self.test_set
         names = full_data.columns 
         aic_hold = []
         obs = len(full_data)/60
+        one_percent = int(obs * .01)
+        percent = 1
         min_row = 0 
         max_row = 60
         for x in range(0, int(obs)): 
@@ -85,7 +96,10 @@ class SubmitCreature():
                 #     warnings.simplefilter("ignore")
                 #     model = tsmodel.ARIMA(test_data, (int(parameter[1]), int(parameter[4]), int(parameter[7])))
                 #     aic = model.fit(disp=False).aic
-                aic = self.arima(robjects.FloatVector(test_data), robjects.IntVector([int(parameter[1]), int(parameter[4]), int(parameter[7])]))[5][0] 
+                try: 
+                    aic = self.arima(robjects.FloatVector(test_data), robjects.IntVector([int(parameter[1]), int(parameter[4]), int(parameter[7])]))[5][0] 
+                except RRuntimeError:
+                    aic = 0
                 none_aic_sd = (aic - self.time_mean_sd['Mean 0'][index])/self.time_mean_sd['SD 0'][index]
                 solar_aic_sd = (aic - self.time_mean_sd['Mean 1'][index])/self.time_mean_sd['SD 1'][index]
                 if(name == 'R_VALUE'): 
@@ -100,4 +114,7 @@ class SubmitCreature():
             aic_hold.append(aic_row)
             min_row = min_row + 60
             max_row = max_row + 60
-        return(np.array(aic_hold))
+            if x % one_percent is 0 and x is not 0: 
+                print("{}%".format(percent))
+                percent += 1
+        self.transformed_data = np.array(aic_hold)
